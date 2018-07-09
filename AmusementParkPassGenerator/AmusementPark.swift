5d4b575f6366a2c837cadac862646e24d4ba4c04
mfukuoka <thechemist@macmini.home>
2018-07-08 19:29:32 -0700

    test cases completed

--- AmusementParkPassGenerator/AmusementPark.swift
+++ AmusementParkPassGenerator/AmusementPark.swift
@@ -9,32 +9,30 @@
 import Foundation
 
 struct AmusementPark {
-
+    
     //register an entrant
-    static func registerEntrant(_ type: EntrantType, _ subType: EntrantSubType, requiredInformation: [RequiredInformation : Any]) throws -> Entrant{
-        var pass = Pass(areaAccess: [.amusement], rideAccess: nil, discountAccess: []) //everyabody getta ride a ride
+    static func registerEntrant(_ type: EntrantType, _ subType: EntrantSubType, requiredInformation: [RequiredInformation : String?]) throws -> Entrant {
         
+        var pass = Pass(areaAccess: [.amusement], rideAccess: nil, discountAccess: [:]) //everyabody getta ride a ride
+        var information: [RequiredInformation : String] = [ : ]
         //type is a guest
         if type == .guest {
             
             //classic guest
-            if subType == .classic {
+            switch subType {
+            case .classic:
                 pass.rideAccess = [.allRides]
-            }
-            
-            //vip guest
-            if subType == .vip {
-                
+            case .vip:
                 pass.rideAccess = [.allRides, .skipLines]
-                pass.discountAccess = [[.food : 10.0],[.merchandise : 25.0 ]]
-            }
-            
-            //child guest
-            if subType == .child {
+                pass.discountAccess[.food] = 10.0
+                pass.discountAccess[.merchandise] = 25.0
+            case .child:
                 //check if a dob was given
                 guard let dob = requiredInformation[.dob] as? String else {
                     throw RegistrationError.dob
                 }
+                information[.dob] = dob
+
                 //attempt to format the date
                 let dateFormatter = DateFormatter()
                 dateFormatter.dateFormat = "yyyy-MM-dd"
@@ -51,16 +49,17 @@ struct AmusementPark {
                         }
                     }
                     else {
-                        throw RegistrationError.dob
+                        throw RegistrationError.verbose(message: "AmusementPark.registerEntrant(): Error - issue getting year from requiredInformation[.dob]")
                     }
                 }
                 else {
                     throw RegistrationError.dob
                 }
-            }
-            else {
+            default:
                 throw RegistrationError.subTypeNotFound
             }
+            
+            
         }
         //type is an employee
         if type == .employee {
@@ -68,68 +67,92 @@ struct AmusementPark {
             guard let dob = requiredInformation[.dob] as? String else {
                 throw RegistrationError.dob
             }
+         
+            information[.dob] = dob
             guard let firstName = requiredInformation[.firstName] as? String else {
                 throw RegistrationError.firstName
             }
+            information[.firstName] = firstName
             guard let lastName = requiredInformation[.lastName] as? String else {
                 throw RegistrationError.lastName
             }
+            information[.lastName] = lastName
             guard let streetAddress = requiredInformation[.streetAddress] as? String else {
                 throw RegistrationError.streetAddress
             }
+            information[.streetAddress] = streetAddress
             guard let city = requiredInformation[.city] as? String else {
                 throw RegistrationError.city
             }
+            information[.city] = city
             guard let state = requiredInformation[.state] as? String else {
                 throw RegistrationError.state
             }
+            information[.state] = state
             guard let zipCode = requiredInformation[.zipCode] as? String else {
                 throw RegistrationError.zipCode
             }
+            information[.zipCode] = zipCode
             guard let ssn = requiredInformation[.ssn] as? String else {
                 throw RegistrationError.ssn
             }
+            information[.ssn] = ssn
 
-            
-            if subType == .foodService {
+            switch subType {
+                
+            case .foodService:
                 pass.areaAccess.append(.kitchen)
                 pass.rideAccess = [.allRides]
-                pass.discountAccess = [[.food : 15.0],[.merchandise : 25.0 ]]
-            }
-            if subType == .rideService {
+                pass.discountAccess[.food] = 15.0
+                pass.discountAccess[.merchandise] = 25.0
+                
+            case .rideService:
                 pass.areaAccess.append(.rideControl)
                 pass.rideAccess = [.allRides]
-                pass.discountAccess = [[.food : 15.0],[.merchandise : 25.0 ]]
-            }
-            if subType == .maintenance {
+                pass.discountAccess[.food] = 15.0
+                pass.discountAccess[.merchandise] = 25.0
+                
+            case .maintenance:
                 pass.areaAccess.append(.kitchen)
                 pass.areaAccess.append(.rideControl)
                 pass.areaAccess.append(.maintenance)
                 pass.rideAccess = [.allRides]
-                pass.discountAccess = [[.food : 15.0],[.merchandise : 25.0 ]]
-            }
-            if subType == .manager {
+                pass.discountAccess[.food] = 15.0
+                pass.discountAccess[.merchandise] = 25.0
+                
+            case .manager:
                 pass.areaAccess.append(.kitchen)
                 pass.areaAccess.append(.rideControl)
                 pass.areaAccess.append(.maintenance)
                 pass.areaAccess.append(.office)
                 pass.rideAccess = [.allRides]
-                pass.discountAccess = [[.food : 25.0],[.merchandise : 25.0 ]]
+                pass.discountAccess[.food] = 25.0
+                pass.discountAccess[.merchandise] = 25.0
                 guard let managementTier = requiredInformation[.managementTier] as? String else {
-                    throw RegistrationError.managementTier
+                throw RegistrationError.managementTier
                 }
-                
+                information[.managementTier] = managementTier
+            
+            default:
+                throw RegistrationError.subTypeNotFound
             }
+            
         }
         
         /*if type == .vendor {
             
         }*/
+     
         
-        return Entrant(type: type, subType: subType, pass: pass)
+        return Entrant(type: type, subType: subType, pass: pass, information: information)
     }
- 
-    static func rideRide(pass: Pass){
+
+
+   /* static func enterPark(pass: Pass) {
+        
+    }
+
+    static func rideRide(pass: Pass) throws {
         if let pass = pass.rideAccess {
             if pass.count > 0 {
                 //can they skip lines?
@@ -146,13 +169,13 @@ struct AmusementPark {
                 }
             }
             else {
-                print("Please see Entrant Access Rules")
+                throw RideAccessError.seeEntrantAccessRules
             }
         }
         else{
-            print("Please see Entrant Access Rules")
+            throw RideAccessError.seeEntrantAccessRules
         }
         
-    }
+    }*/
 }
 
--- AmusementParkPassGenerator/Pass.swift
+++ AmusementParkPassGenerator/Pass.swift
@@ -12,10 +12,12 @@ enum DiscountAccessType {
     case food
     case merchandise
 }
+
 enum RideAccessType {
     case allRides
     case skipLines
 }
+
 enum AreaAccessType {
     case amusement
     case kitchen
@@ -23,13 +25,31 @@ enum AreaAccessType {
     case maintenance
     case office
 }
-struct Pass {
+
+
+class Pass {
     var areaAccess: [AreaAccessType]
     var rideAccess: [RideAccessType]?
-    var discountAccess: [[DiscountAccessType : Double]]
-    init(areaAccess: [AreaAccessType], rideAccess: [RideAccessType]?, discountAccess: [[DiscountAccessType : Double]]){
+    var discountAccess: [DiscountAccessType : Double]
+    var recentlySwiped: Bool
+    init(areaAccess: [AreaAccessType], rideAccess: [RideAccessType]?, discountAccess: [DiscountAccessType : Double]){
         self.areaAccess = areaAccess
         self.rideAccess = rideAccess
         self.discountAccess = discountAccess
+        self.recentlySwiped = false 
+    }
+    
+    func preventSecondSwipe(seconds: Int) {
+        self.recentlySwiped = true
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+        
+        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+            self.recentlySwiped = false
+        }
     }
 }
+
